4f24dd45f040510f5ce08fa13f0cbeceab2658d0
Khoa Phan <khoaphan89@gmail.com>
2021-09-15 08:00:39 -0500

    Add try/catch to handle request error gracefully

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ const { DISCORD_TOKEN, REFRESH_TIMER } = process.env
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const getFloorPrice = async () => {
-  const floorPrice = (
-    await axios.get(
-      'https://api.opensea.io/api/v1/collections?asset_owner=0xFb2CE50C4c8024E037e6be52dd658E2Be23d93Db&offset=0&limit=300'
-    )
-  ).data[0].stats.floor_price;
+  try {
+    const floorPrice = (
+      await axios.get(
+        'https://api.opensea.io/api/v1/collections?asset_owner=0xFb2CE50C4c8024E037e6be52dd658E2Be23d93Db&offset=0&limit=300'
+      )
+    ).data[0].stats.floor_price;
 
-  return `${floorPrice} ETH`;
+    return `${floorPrice} ETH`;
+  } catch (err) {
+    return err;
+  }
 };
 
 const commands = [{
