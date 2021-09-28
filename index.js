7725968969d9cb1d89f82fa3e2b88ae67df5de15
Khoa Phan <khoaphan89@gmail.com>
2021-09-28 16:27:08 -0500

    Wrap sync calls in try/catch

--- index.js
+++ index.js
@@ -1,68 +1,85 @@
 require('dotenv').config()
 
 const axios = require('axios')
-const { Client, Intents } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Client, Intents } = require('discord.js')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
 
 const { DISCORD_TOKEN, REFRESH_TIMER } = process.env
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 const getFloorPrice = async () => {
   try {
     const floorPrice = (
       await axios.get(
-        'https://api.opensea.io/api/v1/collections?asset_owner=0xFb2CE50C4c8024E037e6be52dd658E2Be23d93Db&offset=0&limit=300'
+        'https://api.opensea.io/api/v1/collections?asset_owner=0xFb2CE50C4c8024E037e6be52dd658E2Be23d93Db&offset=0&limit=300',
       )
-    ).data[0].stats.floor_price;
+    ).data[0].stats.floor_price
 
-    return `${floorPrice} ETH`;
+    return `${floorPrice} ETH`
   } catch (err) {
-    return err;
+    return err
   }
-};
+}
 
-const commands = [{
-  name: 'floor',
-  description: 'Show current land floor price'
-}]; 
+const commands = [
+  {
+    name: 'floor',
+    description: 'Show current land floor price',
+  },
+]
 
-const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
+const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN)
 
-(async () => {
+;(async () => {
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log('Started refreshing application (/) commands.')
 
     await rest.put(
-      Routes.applicationGuildCommands('887116641442357289', '882367467022868570'),
+      Routes.applicationGuildCommands(
+        '887116641442357289',
+        '882367467022868570',
+      ),
       { body: commands },
-    );
+    )
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log('Successfully reloaded application (/) commands.')
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-})();
+})()
 
 client.on('ready', async () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`)
 
-  // Fetch floor price and set as bot activity/status
-  client.user.setActivity(await getFloorPrice(), {type: 'WATCHING'});
+  try {
+    // Fetch floor price and set as bot activity/status
+    client.user.setActivity(await getFloorPrice(), { type: 'WATCHING' })
+  } catch (err) {
+    console.error('Error setting activity', err)
+  }
 
   // Set status at set intervals
   setInterval(async () => {
-    client.user.setActivity(await getFloorPrice(), {type: 'WATCHING'});
-  }, REFRESH_TIMER || 3600000);
-});
+    try {
+      client.user.setActivity(await getFloorPrice(), { type: 'WATCHING' })
+    } catch (err) {
+      console.error('Error setting activity at interval', err)
+    }
+  }, REFRESH_TIMER || 3600000)
+})
 
 client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return
 
   if (interaction.commandName === 'floor') {
-    await interaction.reply(await getFloorPrice());
+    try {
+      await interaction.reply(await getFloorPrice())
+    } catch (err) {
+      console.error('Error responding to floor command', err)
+    }
   }
-});
+})
 
-client.login(DISCORD_TOKEN);
+client.login(DISCORD_TOKEN)
